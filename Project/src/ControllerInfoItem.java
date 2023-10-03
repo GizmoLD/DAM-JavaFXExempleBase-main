ce2878a404489461f97c4d5e4548994f39ad43ad
GizmoLD <brian7071@gmail.com>
2023-10-03 12:58:20 +0200

    1

---
+++ Project/src/ControllerInfoItem.java
@@ -0,0 +1,94 @@
+import java.awt.Color;
+import java.util.HashMap;
+import java.util.Map;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Rectangle;
+
+
+public class ControllerInfoItem {
+    @FXML
+    private ImageView img;
+
+    @FXML
+    private Label title = new Label();
+
+    @FXML
+    private Label text = new Label();
+
+    @FXML
+    private Rectangle color = new Rectangle();
+
+    //
+    //
+    @FXML
+    private Label procesador = new Label();
+
+    @FXML
+    private Label data = new Label();
+
+    @FXML
+    private Label venudes = new Label();
+
+    @FXML
+    private Label any = new Label();
+
+    @FXML
+    private Label tipus = new Label();
+
+    @FXML 
+    private Label descripcio = new Label();
+    //
+    //
+
+    public void setImage(String resourceName) {
+
+        // Obté una referència al recurs dins del .jar
+        ClassLoader classLoader = getClass().getClassLoader();
+        Image image = new Image(classLoader.getResourceAsStream(resourceName));
+
+        // Estableix la imatge a l'ImageView
+        img.setImage(image);
+    }
+
+    public void setTitle(String text) {
+
+        // Estableix el contingut del Label
+        this.title.setText(text);
+    }
+
+    public void setText(String text) {
+
+        // Estableix el contingut del Label
+        this.text.setText(text);
+    }
+
+    //
+    //
+    //
+    public void setData(String data){
+        this.data.setText(data);
+    }
+
+    public void setProcesador(String procesador){
+        this.procesador.setText(procesador);
+    }
+
+    public void setVenudes(int venudes){
+        this.procesador.setText(""+venudes);
+    }
+
+    public void setAny(String any){
+        this.any.setText(any);
+    }
+
+
+
+    //
+    //
+    //
+}
